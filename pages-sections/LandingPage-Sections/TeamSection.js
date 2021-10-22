deb40b524190ab479659d3a4f552201fe3520f97
CodeDestroyer19 <76995924+CodeDestroyer19@users.noreply.github.com>
2021-10-22 17:16:04 +0200

    Simiyu

--- pages-sections/LandingPage-Sections/TeamSection.js
+++ pages-sections/LandingPage-Sections/TeamSection.js
@@ -75,7 +75,7 @@ export default function TeamSection() {
                 />
               </GridItem>
               <h4 className={classes.cardTitle}>
-                Joseph Siminyu Baraza
+                Joseph Simiyu Baraza
                 <br />
                 <small className={classes.smallTitle}>Campus Manager</small>
               </h4>
